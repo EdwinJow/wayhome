9edf9a33f827aedc5207d07172e6bb54b477025b
Edwin Jow <jow.edwin@gmail.com>
2017-06-04 01:14:03 -0400

    Wired basic autocomplete to map for verification of address

--- app/src/main/java/info/truthindata/weatherandtraffic/MainActivity.java
+++ app/src/main/java/info/truthindata/weatherandtraffic/MainActivity.java
@@ -1,100 +1,69 @@
 package info.truthindata.weatherandtraffic;
 
-import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.text.Html;
 import android.text.Spanned;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements PlaceSelectionListener  {
-    public static final String DARK_SKY_API = "info.truthindata.weatherandtraffic.DARK_SKI_API";
-    public static final String HOME_ADDRESS = "info.truthindata.weatherandtraffic.HOME_ADDRESS";
-    public static final String WORK_ADDRESS = "info.truthindata.weatherandtraffic.WORK_ADDRESS";
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, PlaceSelectionListener {
+    private GoogleMap mMap;
     private static final String TAG = "MainActivity";
-    private GoogleApiClient mGoogleApiClient;
-    private TextView mPlaceDetailsText;
-    private TextView mPlaceAttribution;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-        // Retrieve the PlaceAutocompleteFragment.
+       // Retrieve the PlaceAutocompleteFragment.
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+              getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
-        // Register a listener to receive callbacks when a place has been selected or an error has
-        // occurred.
         autocompleteFragment.setOnPlaceSelectedListener(this);
-
-        mPlaceDetailsText = (TextView) findViewById(R.id.place_details);
-        mPlaceAttribution = (TextView) findViewById(R.id.place_attribution);
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-
-        EditText editWork = (EditText) findViewById(R.id.editWorkAddress);
-        EditText editHome = (EditText) findViewById(R.id.editHomeAddress);
-        EditText editDarkSky = (EditText) findViewById(R.id.editDarkSkyApi);
-
-        String darkSkyApi = editDarkSky.getText().toString();
-        String homeAddress = editHome.getText().toString();
-        String workAddress = editWork.getText().toString();
-
-        intent.putExtra(DARK_SKY_API, darkSkyApi);
-        intent.putExtra(HOME_ADDRESS, homeAddress);
-        intent.putExtra(WORK_ADDRESS, workAddress);
-
-        startActivity(intent);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng defaultCoords = new LatLng(40.1623518,-74.9563831);
+        mMap = googleMap;
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(defaultCoords));
     }
 
-    public void onPlaceSelected(Place place) {
-        Log.i(TAG, "Place Selected: " + place.getName());
-
-        // Format the returned place's details and display them in the TextView.
-        mPlaceDetailsText.setText(formatPlaceDetails(getResources(), place.getName(), place.getId(),
-                place.getAddress(), place.getPhoneNumber(), place.getWebsiteUri()));
-
-        CharSequence attributions = place.getAttributions();
-        if (!TextUtils.isEmpty(attributions)) {
-            mPlaceAttribution.setText(Html.fromHtml(attributions.toString()));
-        } else {
-            mPlaceAttribution.setText("");
-        }
-    }
-
-    /**
-     * Helper method to format information about a place nicely.
-     */
     private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
                                               CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+
         Log.e(TAG, res.getString(R.string.place_details, name, id, address, phoneNumber,
                 websiteUri));
         return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
                 websiteUri));
-
     }
 
-    /**
-     * Callback invoked when PlaceAutocompleteFragment encounters an error.
-     */
+    public void onPlaceSelected(Place place) {
+        Log.i(TAG, "Place Selected: " + place.getName());
+        LatLng t = place.getLatLng();
+        mMap.addMarker(new MarkerOptions().position(t).title(place.getName().toString()));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(t, 13));
+        CharSequence attributions = place.getAttributions();
+    }
+
     @Override
     public void onError(Status status) {
         Log.e(TAG, "onError: Status = " + status.toString());
@@ -102,5 +71,4 @@ public class MainActivity extends AppCompatActivity implements PlaceSelectionLis
         Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
                 Toast.LENGTH_SHORT).show();
     }
-
 }
