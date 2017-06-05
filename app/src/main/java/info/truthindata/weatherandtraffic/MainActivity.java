e278ea3963d6d9f4c3d1dfc722963b1d0160949f
Edwin Jow <jow.edwin@gmail.com>
2017-06-05 00:10:12 -0400

    Added second autocomplete and wired up some bounding for both markers

--- app/src/main/java/info/truthindata/weatherandtraffic/MainActivity.java
+++ app/src/main/java/info/truthindata/weatherandtraffic/MainActivity.java
@@ -1,28 +1,30 @@
 package info.truthindata.weatherandtraffic;
 
-import android.content.res.Resources;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.text.Html;
-import android.text.Spanned;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback, PlaceSelectionListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback  {
     private GoogleMap mMap;
     private static final String TAG = "MainActivity";
+    protected List<Marker> markers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +36,64 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
        // Retrieve the PlaceAutocompleteFragment.
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-              getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        PlaceAutocompleteFragment homeAutoComplete = (PlaceAutocompleteFragment)
+              getFragmentManager().findFragmentById(R.id.place_home_autocomplete_fragment);
 
-        autocompleteFragment.setOnPlaceSelectedListener(this);
+        // Retrieve the PlaceAutocompleteFragment.
+        PlaceAutocompleteFragment workAutoComplete = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_work_autocomplete_fragment);
+
+        homeAutoComplete.setHint("Enter your home address");
+        workAutoComplete.setHint("Enter your work address");
+
+        homeAutoComplete.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                LatLng t = place.getLatLng();
+                Log.i(TAG, "Place Selected: " + place.getName());
+
+                Marker marker = mMap.addMarker(new MarkerOptions().position(t).title(place.getName().toString()));
+                markers.add(marker);
+
+                fitBounds();
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.e(TAG, "onError: Status = " + status.toString());
+            }
+        });
+
+        workAutoComplete.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                LatLng t = place.getLatLng();
+                Log.i(TAG, "Place Selected: " + place.getName());
+
+                Marker marker = mMap.addMarker(new MarkerOptions().position(t).title(place.getName().toString()));
+                markers.add(marker);
+
+                fitBounds();
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.e(TAG, "onError: Status = " + status.toString());
+            }
+        });
+    }
+
+    public void fitBounds(){
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (Marker marker : markers) {
+            builder.include(marker.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+
+        int padding = 20; // offset from edges of the map in pixels
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+
+        mMap.animateCamera(cu);
     }
 
     @Override
@@ -45,30 +101,5 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng defaultCoords = new LatLng(40.1623518,-74.9563831);
         mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.newLatLng(defaultCoords));
-    }
-
-    private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
-                                              CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
-
-        Log.e(TAG, res.getString(R.string.place_details, name, id, address, phoneNumber,
-                websiteUri));
-        return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
-                websiteUri));
-    }
-
-    public void onPlaceSelected(Place place) {
-        Log.i(TAG, "Place Selected: " + place.getName());
-        LatLng t = place.getLatLng();
-        mMap.addMarker(new MarkerOptions().position(t).title(place.getName().toString()));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(t, 13));
-        CharSequence attributions = place.getAttributions();
-    }
-
-    @Override
-    public void onError(Status status) {
-        Log.e(TAG, "onError: Status = " + status.toString());
-
-        Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
-                Toast.LENGTH_SHORT).show();
     }
 }
